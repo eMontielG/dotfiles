90c84d9bd9910b05ff77176ac61a5de2ad5d3f28
eMontielG <40529625+eMontielG@users.noreply.github.com>
2024-12-31 19:45:03 -0600

    add nvim configuration file
    Initial nvim setup with basic tabbing and leader key preferences.

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,9 @@
+vim.g.mapleader = " "
+vim.g.maplocalleaer = "\\"
+
+vim.opt.expandtab = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
