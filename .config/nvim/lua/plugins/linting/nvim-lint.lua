0b0e92db5abafb5cd80bd3d3386640236ded1013
eMontielG <40529625+eMontielG@users.noreply.github.com>
2025-01-03 22:45:31 -0600

    feat: neovim add linting capabilities
    Configuration for linting in neovim, starting with Lua.

---
+++ .config/nvim/lua/plugins/linting/nvim-lint.lua
@@ -0,0 +1,14 @@
+return {
+	"mfussenegger/nvim-lint",
+	config = function()
+		local linter = require("lint")
+		linter.linters_by_ft = {
+			lua = { "luacheck" },
+		}
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "CmdlineEnter" }, {
+			callback = function()
+				linter.try_lint()
+			end,
+		})
+	end,
+}
