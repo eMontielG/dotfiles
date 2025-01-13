1059a34847425498715e941d41688b81a14ae5be
eMontielG <40529625+eMontielG@users.noreply.github.com>
2025-01-12 21:33:17 -0600

    feat: neovim add completion capabilities to lua
    Setup completion for Lua language.

--- .config/nvim/lua/plugins/lsp/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/nvim-lspconfig.lua
@@ -1,11 +1,17 @@
 return {
 	"neovim/nvim-lspconfig",
+	dependencies = { "saghen/blink.cmp" },
 	config = function()
-		require("lspconfig").lua_ls.setup({
+		local capabilities = require("blink.cmp").get_lsp_capabilities()
+		local lspconfig = require("lspconfig")
+
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
 			on_attach = function(client)
 				client.server_capabilities.semanticTokensProvider = nil
 			end,
 		})
+
 		vim.keymap.set({ "n" }, "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
 		vim.keymap.set({ "n" }, "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
 	end,
