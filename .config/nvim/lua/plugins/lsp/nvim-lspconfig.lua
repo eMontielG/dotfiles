5f210cfdb5c28679a9c64e121774ee8bda0359a9
eMontielG <40529625+eMontielG@users.noreply.github.com>
2025-01-13 19:10:24 -0600

    neovim add lua_ls vim support
    Add Vim types and definitions to the Lua Language Server for better completion.

--- .config/nvim/lua/plugins/lsp/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/nvim-lspconfig.lua
@@ -1,6 +1,6 @@
 return {
 	"neovim/nvim-lspconfig",
-	dependencies = { "saghen/blink.cmp" },
+	dependencies = { { "saghen/blink.cmp" }, { "folke/lazydev.nvim" } },
 	config = function()
 		local capabilities = require("blink.cmp").get_lsp_capabilities()
 		local lspconfig = require("lspconfig")
