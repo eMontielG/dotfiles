c363eb182b25e53d60c3c3fb2401e2e5674abd88
eMontielG <40529625+eMontielG@users.noreply.github.com>
2025-02-07 13:22:26 -0600

    enable lsp highlighting for lua

--- .config/nvim/lua/plugins/lsp/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/nvim-lspconfig.lua
@@ -5,12 +5,7 @@ return {
 		local capabilities = require("blink.cmp").get_lsp_capabilities()
 		local lspconfig = require("lspconfig")
 
-		lspconfig.lua_ls.setup({
-			capabilities = capabilities,
-			on_attach = function(client)
-				client.server_capabilities.semanticTokensProvider = nil
-			end,
-		})
+		lspconfig.lua_ls.setup({ capabilities = capabilities })
 
 		vim.keymap.set({ "n" }, "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
 		vim.keymap.set({ "n" }, "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
