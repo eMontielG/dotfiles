3f0cf9076c656f2fac37f1ef75b1194dbf1034f4
eMontielG <40529625+eMontielG@users.noreply.github.com>
2025-02-13 20:41:47 -0600

    neovim fuzzy finder preview ui

--- .config/nvim/lua/plugins/navigation/fzf-lua.lua
+++ .config/nvim/lua/plugins/navigation/fzf-lua.lua
@@ -1,6 +1,15 @@
 return {
 	"ibhagwan/fzf-lua",
 	config = function()
+		require("fzf-lua").setup({
+			winopts = {
+				border = "none",
+				fullscreen = true,
+				preview = {
+					border = "none",
+				},
+			},
+		})
 		vim.keymap.set({ "n" }, "<leader>ff", "<cmd>lua require('fzf-lua').files()<CR>")
 	end,
 }
