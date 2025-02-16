29888e7cee93e520b99b29d4647e8228fe91d4c9
eMontielG <40529625+eMontielG@users.noreply.github.com>
2025-02-16 17:24:51 -0600

    neovim explorer options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,8 +1,14 @@
 vim.g.mapleader = " "
 vim.g.maplocalleaer = "\\"
 
+vim.g.netrw_banner = 0
+vim.g.netrw_keepdir = 0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_winsize = 25
+
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.scrolloff = 16
 vim.opt.shiftwidth = 2
 vim.opt.signcolumn = "yes:1"
 vim.opt.softtabstop = 2
