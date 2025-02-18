4087cb8db4f92d92ad2409b2597e92f2c38a14f4
eMontielG <40529625+eMontielG@users.noreply.github.com>
2025-02-18 14:13:38 -0600

    neovim treesitter required parameters

--- .config/nvim/lua/plugins/highlighting/treesitter.lua
+++ .config/nvim/lua/plugins/highlighting/treesitter.lua
@@ -3,14 +3,19 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter.configs").setup({
+			auto_install = false,
 			ensure_installed = {
 				"css",
 				"javascript",
 				"lua",
+				"query",
 				"typescript",
 			},
 			highlight = { enable = true },
+			ignore_install = {},
 			indent = { enable = false },
+			modules = {},
+			sync_install = true,
 		})
 	end,
 }
