32c5b8563c2e0d18eab697cc20c00c28a5698d0b
eMontielG <40529625+eMontielG@users.noreply.github.com>
2025-02-27 18:05:53 -0600

    remove fuzzy finder plugin
    Stripping away fzf-lua, will instead use native find and grep.

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -4,7 +4,6 @@ require("lazy").setup({
 	{ import = "plugins.highlighting" },
 	{ import = "plugins.linting" },
 	{ import = "plugins.lsp" },
-	{ import = "plugins.navigation" },
 	{ import = "plugins.ui" },
 	{ import = "plugins.theming" },
 	{ import = "plugins.tooling" },
