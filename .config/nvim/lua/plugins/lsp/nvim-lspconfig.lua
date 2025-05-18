378b554e94a54079a379a3b35e9049630011abc5
eMontielG <40529625+eMontielG@users.noreply.github.com>
2025-05-17 20:24:30 -0600

    neovim update lspconfig

--- .config/nvim/lua/plugins/lsp/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/nvim-lspconfig.lua
@@ -1,12 +1,11 @@
 return {
 	"neovim/nvim-lspconfig",
-	dependencies = { { "saghen/blink.cmp" }, { "folke/lazydev.nvim" } },
+	dependencies = { { "folke/lazydev.nvim" } },
 	config = function()
 		local lspconfig = require("lspconfig")
-		local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-		lspconfig.cssls.setup({ capabilities = capabilities })
-		lspconfig.lua_ls.setup({ capabilities = capabilities })
-		lspconfig.ts_ls.setup({ capabilities = capabilities })
+		lspconfig.cssls.setup({})
+		lspconfig.lua_ls.setup({})
+		lspconfig.ts_ls.setup({})
 	end,
 }
